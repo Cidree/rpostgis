44267751c25ece536c9573069bfec154cc022bba
Mathieu Basille <basille@ase-research.org>
2016-08-06 17:05:49 -0400

    pgMakePts/Stp + check(rpostgis).

--- R/pgMakePts.R
+++ R/pgMakePts.R
@@ -10,10 +10,6 @@
 ##'     geometry column.
 ##' @param x The name of the x/longitude field.
 ##' @param y The name of the y/latitude field.
-##' @param dx The name of the dx field (i.e. increment in x
-##'     direction).
-##' @param dy The name of the dy field (i.e. increment in y
-##'     direction).
 ##' @param srid A valid SRID for the new geometry.
 ##' @param index Logical. Whether to create an index on the new
 ##'     geometry.
@@ -35,11 +31,6 @@
 ##' ## Create a new POINT field called 'pts_geom'
 ##' pgMakePts(conn, name = c("schema", "table"), colname = "pts_geom",
 ##'     x = "longitude", y = "latitude", srid = 4326, exec = FALSE)
-##'
-##' ## Create a new LINESTRING field called 'stp_geom'
-##' pgMakeStp(conn, name = c("schema", "table"), colname = "stp_geom",
-##'     x = "longitude", y = "latitude", dx = "xdiff", dy = "ydiff",
-##'     srid = 4326, exec = FALSE)
 
 pgMakePts <- function(conn, name, colname = "geom", x = "x",
     y = "y", srid, index = TRUE, display = TRUE, exec = TRUE) {
@@ -89,11 +80,11 @@ pgMakePts <- function(conn, name, colname = "geom", x = "x",
     ## SQL query to populate the POINT geometry field
     ## --
     ## UPDATE "<schema>"."<table>"
-    ## SET "<colname>"=ST_SetSRID(ST_MakePoint("<x>", "<y>"), <srid>)
+    ## SET "<colname>" = ST_SetSRID(ST_MakePoint("<x>", "<y>"), <srid>)
     ## WHERE "<x>" IS NOT NULL AND "<y>" IS NOT NULL;
     ## --
     tmp.query <- paste0("UPDATE ", nameque, "\nSET ", colnameque,
-        "=ST_SetSRID(ST_MakePoint(", x, ", ", y, "), ", srid,
+        " = ST_SetSRID(ST_MakePoint(", x, ", ", y, "), ", srid,
         ")\nWHERE ", x, " IS NOT NULL AND ", y, " IS NOT NULL;")
     ## Display the query
     if (display) {
@@ -112,9 +103,19 @@ pgMakePts <- function(conn, name, colname = "geom", x = "x",
 
 ## pgMakeStp
 
-## @rdname pgMakePts
-## @importFrom DBI dbQuoteIdentifier
-## @export
+##' @rdname pgMakePts
+##' @param dx The name of the dx field (i.e. increment in x
+##'     direction).
+##' @param dy The name of the dy field (i.e. increment in y
+##'     direction).
+##' @importFrom DBI dbQuoteIdentifier
+##' @export
+##' @examples
+##'
+##' ## Create a new LINESTRING field called 'stp_geom'
+##' pgMakeStp(conn, name = c("schema", "table"), colname = "stp_geom",
+##'     x = "longitude", y = "latitude", dx = "xdiff", dy = "ydiff",
+##'     srid = 4326, exec = FALSE)
 
 pgMakeStp <- function(conn, name, colname = "geom", x = "x",
     y = "y", dx = "dx", dy = "dy", srid, index = TRUE, display = TRUE,
@@ -166,14 +167,14 @@ pgMakeStp <- function(conn, name, colname = "geom", x = "x",
     ## SQL query to populate the LINESTRING geometry field
     ## --
     ## UPDATE "<schema>"."<table>"
-    ## SET "<colname>"=ST_SetSRID(ST_MakeLine(
+    ## SET "<colname>" = ST_SetSRID(ST_MakeLine(
     ##     ARRAY[ST_MakePoint("<x>", "<y>"),
     ##           ST_MakePoint("<x>" + "<dx>", "<y>" + "<dy>")]
     ##     ), <srid>)
     ## WHERE "<x>" IS NOT NULL AND "<y>" IS NOT NULL;
     ## --
     tmp.query <- paste0("UPDATE ", nameque, "\nSET ", colnameque,
-        "=ST_SetSRID(ST_MakeLine(\n    ARRAY[ST_MakePoint(",
+        " = ST_SetSRID(ST_MakeLine(\n    ARRAY[ST_MakePoint(",
         x, ", ", y, "), ", "\n          ST_MakePoint(", x, " + ",
         dx, ", ", y, " + ", dy, ")]\n    ), ", srid, ")\nWHERE ",
         dx, " IS NOT NULL AND ", dy, " IS NOT NULL;")
