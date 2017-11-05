1a73ebdf6d5e15d58ebea9756b70f83b9e0d125b
Mathieu Basille <basille@ufl.edu>
2017-11-05 17:07:43 -0500

    dbAddKey: Examples and nameque.

--- R/dbAddKey.R
+++ R/dbAddKey.R
@@ -33,22 +33,27 @@
 ##' @examples
 ##' ## Examples use a dummy connection from DBI package
 ##' conn <- DBI::ANSI()
+##'
 ##' ## Primary key
 ##' dbAddKey(conn, name = c("sch1", "tbl1"), colname = "id1", exec = FALSE)
+##'
 ##' ## Primary key using multiple columns
-##' dbAddKey(conn, name = c("sch1", "tbl1"), colname = c("id1", "id2"),
-##'     exec = FALSE)
+##' dbAddKey(conn, name = c("sch1", "tbl1"), colname = c("id1", "id2",
+##'     "id3"), exec = FALSE)
+##'
 ##' ## Foreign key
 ##' dbAddKey(conn, name = c("sch1", "tbl1"), colname = "id", type = "foreign",
 ##'     reference = c("sch2", "tbl2"), colref = "id", exec = FALSE)
+##'
 ##' ## Foreign key using multiple columns
 ##' dbAddKey(conn, name = c("sch1", "tbl1"), colname = c("id1", "id2"),
 ##'     type = "foreign", reference = c("sch2", "tbl2"), colref = c("id3",
 ##'         "id4"), exec = FALSE)
 dbAddKey <- function(conn, name, colname, type = c("primary",
     "foreign"), reference, colref, display = TRUE, exec = TRUE) {
-    ## Check and prepare the schema.name
-    name <- paste(dbTableNameFix(conn, name), collapse = ".")
+    ## Check and prepare the schema.name and column name
+    name <- dbTableNameFix(conn, name)
+    nameque <- paste(name, collapse = ".")
     colname <- paste(DBI::dbQuoteIdentifier(conn, colname), collapse = ", ")
     ## Check 'type' and set it to upper case
     type <- toupper(match.arg(type))
@@ -66,7 +71,7 @@ dbAddKey <- function(conn, name, colname, type = c("primary",
             collapse = "."), " (", colref, ")")
     }
     ## Build the query
-    tmp.query <- paste0("ALTER TABLE ", name, " ADD ", type,
+    tmp.query <- paste0("ALTER TABLE ", nameque, " ADD ", type,
         " KEY (", colname, ")", references, ";")
     ## Display the query
     if (display) {
