364f41b69ed3a6e3c90a76980af42c2bb4aa1981
David Bucklin <david.bucklin@gmail.com>
2023-04-28 09:07:20 -0400

    add startup file

---
+++ R/startup.R
@@ -0,0 +1,14 @@
+# Startup message
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(
+    "This is 'rpostgis' version ", utils::packageVersion("rpostgis"), " (",
+    utils::packageDate("rpostgis"),
+    ").\n\n",
+    "Due to retirement of the package 'rgeos', 'rpostgis' will retire on September 2023.\n",
+    "  * For vector operations, please check package 'sf', which provides a mechanism to connect to PostGIS databases.\n",
+    "  * For raster operations, no alternative solution is identified yet.\n",
+    "  * For general database operations, use 'RPostgreSQL' directly.\n\n",
+    "If you are interested in the development and maintenance of 'rpostgis', please check:\n",
+    "https://github.com/mablab/rpostgis/issues/28"
+  )
+}
