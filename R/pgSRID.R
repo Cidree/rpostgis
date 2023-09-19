2294d4b3f8afc3749c36f81d58b68eccd1151255
Cidree <adrian.cidre@gmail.com>
2023-09-19 22:06:57 +0200

    Update examples

--- R/pgSRID.R
+++ R/pgSRID.R
@@ -31,9 +31,9 @@
 ##' drv <- dbDriver("PostgreSQL")
 ##' conn <- dbConnect(drv, dbname = "dbname", host = "host", port = "5432",
 ##'     user = "user", password = "password")
-##' (crs <- CRS("+proj=longlat"))
+##' (crs <- sf::st_crs("+proj=longlat"))
 ##' pgSRID(conn, crs)
-##' (crs2 <- CRS(paste("+proj=stere", "+lat_0=52.15616055555555 +lon_0=5.38763888888889",
+##' (crs2 <- sf::st_crs(paste("+proj=stere", "+lat_0=52.15616055555555 +lon_0=5.38763888888889",
 ##'     "+k=0.999908 +x_0=155000 +y_0=463000", "+ellps=bessel",
 ##'     "+towgs84=565.237,50.0087,465.658,-0.406857,0.350733,-1.87035,4.0812",
 ##'     "+units=m")))
