d436b453937777d41a427758c0f495cad5515c3e
Cidree <adrian.cidre@gmail.com>
2023-09-20 11:32:50 +0200

    Update startup message

--- R/startup.R
+++ R/startup.R
@@ -3,12 +3,6 @@
   packageStartupMessage(
     "This is 'rpostgis' version ", utils::packageVersion("rpostgis"), " (",
     utils::packageDate("rpostgis"),
-    ").\n\n",
-    "Due to retirement of the package 'rgeos', 'rpostgis' will retire in September 2023.\n",
-    "  * For vector operations, please check package 'sf', which provides a mechanism to connect to PostGIS databases.\n",
-    "  * For raster operations, no alternative solution is identified yet.\n",
-    "  * For general database operations, use 'RPostgreSQL' directly.\n\n",
-    "If you are interested in the development and maintenance of 'rpostgis', please check:\n",
-    "https://github.com/mablab/rpostgis/issues/28"
+    ")"
   )
 }
