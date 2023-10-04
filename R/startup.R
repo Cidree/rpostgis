54c40bb1f27d6ed275c5f99380cd2f5259280681
Cidree <adrian.cidre@gmail.com>
2023-10-04 11:28:53 +0200

    update startup message for 1.5

--- R/startup.R
+++ R/startup.R
@@ -2,7 +2,8 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(
     "This is 'rpostgis' version ", utils::packageVersion("rpostgis"), " (",
-    utils::packageDate("rpostgis"),
-    ")"
+    utils::packageDate("rpostgis"), ") \n",
+    "Support for `sp` and `raster` objects will be deprecated in {rpostgis} and will be removed ",
+    "in a future release of the package. Please use `sf` and `terra` objects with {rpostgis}"
   )
 }
