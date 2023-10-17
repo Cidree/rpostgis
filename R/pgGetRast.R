9676edcde357e73e6a89383eb829bc05745eef12
Cidree <adrian.cidre@gmail.com>
2023-10-17 16:43:22 +0200

    fix get raster issue

--- R/pgGetRast.R
+++ R/pgGetRast.R
@@ -116,7 +116,7 @@ pgGetRast <- function(conn, name, rast = "rast", bands = 1,
   p4s <- NA
   # tmp.query <- paste0("SELECT proj4text AS p4s FROM spatial_ref_sys WHERE srid = ",
   #                     srid$st_srid, ";")
-  tmp.query.sr <- try(paste0("SELECT r_proj4 AS p4s FROM ", nameque, ";"))
+  try(tmp.query.sr <- paste0("SELECT r_proj4 AS p4s FROM ", nameque, ";"))
   if (!exists("tmp.query.sr")) tmp.query.sr <- paste0("SELECT proj4text AS p4s FROM spatial_ref_sys WHERE srid = ",
                                                 srid$st_srid, ";")
   db.proj4 <- dbGetQuery(conn, tmp.query.sr)$p4s
